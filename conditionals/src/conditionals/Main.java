56a3329f7047d7266517208b54549928779dbd85
Dilara Bedir <dilara_bedir@hotmail.com>
2022-09-20 21:40:48 +0300

    first commit

---
+++ conditionals/src/conditionals/Main.java
@@ -0,0 +1,31 @@
+package conditionals;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi = 20;
+		if (sayi < 20) {
+			System.out.println("Sayý 20'den küçüktür.");
+		}
+		// if(sayi < 15){
+		// System.out.println("Sayý 15'den küçüktür.");
+		// }
+		else {
+			System.out.println("Sayý 20'den büyüktür.");
+		}
+		// tek eþittir sayýya deðer atamak demektir.
+		// < (küçüktür), > (büyüktür), == (eþittir) operatörleri,
+		if (sayi < 20) {
+			System.out.println("Sayý 20'den küçüktür.");
+
+		} else if (sayi == 20) {
+			System.out.println("Sayý 20'ye eþittir.");
+		}
+		// diðer durumlarý içerir
+		else {
+			System.out.println("Sayý 20'den büyüktür.");
+		}
+
+	}
+
+}
